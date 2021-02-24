81ba646991c6db7f717a070f74b5e77c08cc541d
Serad, Basam C <58251967+DeJaVu1223@users.noreply.github.com>
2021-02-24 10:58:18 +0800

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 var level = require('level')
  
 var db = level('./student-db')
+const prompt = require('prompt-sync')({sigint: true});
+
+  let inputAgain = true;
+
+  while (inputAgain) {
+      // Get user input
+      var id = prompt('Enter your Id Number: ');
+      var fullName = prompt('Enter Fullname: ');
+      var age =  prompt('Enter your Age: ')
+      var address = prompt('Enter your Address: ')
+      acceptStudents (id,fullName,age,address)
+      input = prompt('You want to input again (y/n)?: ');
+      if (input == 'y') {
+          inputAgain = true;
+      } else if (input == 'n') {
+          inputAgain = false;
+      }
+  }
+
+
+ 
+function acceptStudents (id,fullName,age,address){
+
+console.log('ID: ', Number(id));
+console.log(`Name:  `, fullName);
+console.log('Age: ', Number(age))
+console.log('Address: ', address)
+}
